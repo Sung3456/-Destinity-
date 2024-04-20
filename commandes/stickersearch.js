db88c20e1ef6aba23e109d8f1d86cc0279a4bc03
Sung3456 <166514281+Sung3456@users.noreply.github.com>
2024-04-20 13:32:41 +0000

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'DEXTER-MD',
+      author: '☆♤Destinity☆♤',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
