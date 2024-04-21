c2f90e1054493ebfbc38cefbc7468d1505cf4ca6
Sung3456 <166514281+Sung3456@users.noreply.github.com>
2024-04-21 12:59:33 +0000

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -73,7 +73,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     }
   });
   
-  zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "gpt4", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
@@ -83,7 +83,8 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const question = arg.join(' ');
-      const response = `https://ultimetron.guruapi.tech/gpt3?prompt=${question}`;
+      const response = await (await fetch(`https://api.maher-zubair.tech/ai/chatgptv4?q=${text}`)).json();
+
       
       const data = response.data;
       if (data) {
